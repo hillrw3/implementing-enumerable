07aa11716a7f5867f7e36a4394d78cc5de66c8de
Rob Hill <robert.hill0122@gmail.com>
2014-07-31 09:53:32 -0600

    complete warmup

--- lib/gschool_enumerable.rb
+++ lib/gschool_enumerable.rb
@@ -1,3 +1,48 @@
 module GschoolEnumerable
 
+  def g_each
+    for item in self
+      yield(item)
+    end
+    return self
+  end
+
+  def g_select
+    ary = []
+    self.g_each do |item|
+      ary << item if yield(item)
+    end
+    ary
+  end
+
+  def g_reject
+    ary = []
+    self.g_each do |item|
+      ary << item if yield(item) == false
+    end
+    ary
+  end
+
+  def g_map
+    ary = []
+    self.g_each do |item|
+      ary << yield(item)
+    end
+    ary
+  end
+
+  def g_find
+    ary = []
+    self.g_each do |item|
+      ary << item if yield(item)
+    end
+    ary.first
+  end
+
+  def g_select!
+    self.g_each do |item|
+      self.delete(item) unless yield(item)
+    end
+  end
+
 end
